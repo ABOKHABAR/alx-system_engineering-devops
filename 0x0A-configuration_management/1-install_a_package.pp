ae5066cff9ae52de98f34a33c56d933e9cecd1f6
ABOKHABAR <amossab89@gmail.com>
2024-03-22 15:36:37 -0700

    abokhabr

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Using Puppet, install flask from pip3
+# Using Puppet, install flask and Werkzeug 2.1.1 from pip3
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
